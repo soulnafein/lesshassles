44b9d6a12c744592eca56fe253cf192726eee2b4
David Santoro <soulnafein@gmail.com>
2009-04-15 00:01:04 +0100

    Trying to fix git repository

--- src/main/java/com/lesshassles/web/TasksController.java
+++ src/main/java/com/lesshassles/web/TasksController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
